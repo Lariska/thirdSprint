08c69204d380b122b8f49ee0f81b9871451f1970
Fima <FimaRozovsky@hotmail.com>
2017-06-22 10:56:10 +0300

    added email + defined its route in the router

---
+++ src/components/email/Email.vue
@@ -0,0 +1,18 @@
+<template>
+<h1>HI!</h1> 
+</template>
+
+<script>
+export default {
+  name: 'Email',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
